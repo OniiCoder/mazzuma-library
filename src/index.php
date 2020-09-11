75c05fb1280c1c2087d8a079c182abcee15207ca
Peter Perez <peterperez@Peters-MacBook-Pro.local>
2020-09-11 19:50:59 +0100

    Modify api response message when there's no apikey

--- src/index.php
+++ src/index.php
@@ -6,11 +6,13 @@ class Index {
 
     public function sendMoney($endpoint_url, $transaction_data) {
 
+        // check if an api key is provided
        if($transaction_data["apikey"] == null || $transaction_data["apikey"] == '') {
-            $response->code = 500;
-            $response->status = "failed";
+            $response->code = 400;
+            $response->status = "Please enter your apikey";
             return json_encode($response);
        } else {
+
         $additional_headers = array(
             'Content-Type: application/json'
          );
