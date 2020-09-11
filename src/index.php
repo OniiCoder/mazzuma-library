b4159460e09db52b5efe1e7f84655d7c599d8e1b
Peter Perez <peterperez@Peters-MacBook-Pro.local>
2020-09-11 19:08:17 +0100

    Update Composer package

--- src/index.php
+++ src/index.php
@@ -6,4 +6,27 @@ class Index {
     public function greet($greet = "Hello World") {
         return $greet;
     }
+
+    function sendMoney($endpoint_url, $transaction_data) {
+
+        ($endpoint_url == '') ? $endpoint_url = 'https://client.teamcyst.com/api_call.php' : null;
+
+       if($transaction_data["apikey"] == null || $transaction_data["apikey"] == '') {
+            $response->code = 500;
+            $response->status = "failed";
+            return json_encode($response);
+       } else {
+        $additional_headers = array(
+            'Content-Type: application/json'
+         );
+
+         $ch = curl_init($endpoint_url);                                                                      
+         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($transaction_data));                                                                 
+         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+         curl_setopt($ch, CURLOPT_HTTPHEADER, $additional_headers); 
+         
+         return $server_output = curl_exec ($ch);
+       }
+    }
 }
