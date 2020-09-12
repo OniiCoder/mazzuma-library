5e52d2d8b480e34303c660a79ff4821b2bb60f49
Peter Perez <peterperez@Peters-MacBook-Pro.local>
2020-09-12 08:42:13 +0100

    handle input validation

--- src/index.php
+++ src/index.php
@@ -3,16 +3,57 @@
 namespace Peter\Mazzuma;
 
 class MazzumaAPI {
+     //TODO
+    //Validate amount
+    //Validate fields
 
-    public function sendMoney($endpoint_url, $transaction_data) {
+    /** @var string The API Endpoint */
+    private $api = 'https://client.teamcyst.com/api_call.php';
 
-        // check if an apikey is provided
-       if($transaction_data["apikey"] == null || $transaction_data["apikey"] == '') {
-            // if no apikey was provided, return a "bad request" response
-            $response->code = 400;
-            $response->status = "Please enter your apikey";
-            return json_encode($response);
-       } else {
+    /** @var string The API Key from Mazzuma */
+    private $apikey;
+
+    // /** @var string An integer value of the amount being sent */
+    // private $amount;
+
+    // /** @var string Network of sender */
+    // private $network;
+
+    // /** @var string Who is receiving the money? */
+    // private $recipient_momo_number;
+
+    // /** @var string Who is sending the money? */
+    // private $sender_momo_number;
+
+    // /** @var string The transaction flow direction */
+    // private $option;
+
+    /**
+     * create new MazzumaAPI instance
+     */
+    public function __construct($key) {
+        $this->apikey = $key;
+    }
+
+    /**
+     * Call method to process transaction
+     */
+
+    public function sendMoney($data) {
+
+        // check if CURL is enabled
+        if (!function_exists('curl_version')) {
+            return "CURL isn't enabled on your Server !";
+        }
+
+        $transaction_data = $this->buildTransactionDetails(
+            $data['option'],
+            $data['sender_network'],
+            $data['api_key'],
+            $data['sender_momo_number'],
+            $data['recipient_momo_number'],
+            $data['amount']
+        );
 
         $headers = array(
             'Content-Type: application/json'
@@ -24,7 +65,35 @@ class MazzumaAPI {
          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
          curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
          
-         return $server_response = curl_exec ($ch); // return server response
-       }
+         return $server_response = curl_exec ($ch);
+    }
+
+    /**
+     * Parses the Transaction Details into Json for API call
+     */
+    private function buildTransactionDetails(
+        $option,
+        $network,
+        $apikey,
+        $sender_momo_number,
+        $recipient_momo_number,
+        $amount
+    ) {
+        if (empty($option) || empty($network) || empty($apikey) || empty($sender_momo_number) || empty($recipient_momo_number) || empty($amount)) {
+            return "Invalid Input! Make sure to provide all inputs";
+        }
+
+        $data = [
+            "price"=> $amount,
+            "network"=> $network,
+            "recipient_number"=> $paymentDirectionalFlow,
+            "sender"=> $sender_momo_number,
+            "option"=> $option,
+            "apikey"=> $apikey
+        ];
+
+        $json_data = json_encode($data);
+
+        return $json_data;
     }
 }
